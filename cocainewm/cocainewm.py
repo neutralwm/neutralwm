53996bf7229a539fa98360d43d90721d3340dfa2
i <mao@q.Home>
2018-10-30 19:07:39 +0000

    fixed resizing error with double borders

--- cocainewm/cocainewm.py
+++ cocainewm/cocainewm.py
@@ -6,6 +6,8 @@ import subprocess
 double_border = True
 double_border_colour = "424365"
 
+mouse_move = "drag"
+
 dpy = Display()
 
 # ascend window
@@ -19,6 +21,10 @@ dpy.screen().root.grab_key(dpy.keysym_to_keycode(XK.string_to_keysym("Z")), X.Mo
 dpy.screen().root.grab_button(1, X.Mod1Mask, 1, X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE)
 # resize window
 dpy.screen().root.grab_button(3, X.Mod1Mask, 1, X.ButtonPressMask|X.ButtonReleaseMask|X.PointerMotionMask,X.GrabModeAsync, X.GrabModeAsync, X.NONE, X.NONE)
+
+def new_borders():
+	subprocess.call("sleep 0.2; chwb -s 8 -c 0xafafaf $(lsw)", shell=True)
+	subprocess.call("chwb2 -i 1 -o 9 -I " + double_border_colour + " -O $(xrdb -query | head -n 2 | tail -n 1 | cut -c 16-) $(lsw)", shell=True)
 
 startbutton = None
 startkey = None
@@ -43,25 +49,25 @@ while 1:
 			script_path = os.path.dirname(os.path.realpath(__file__)) + '/draw_term'
 			subprocess.call(script_path)
 			if double_border:
-				subprocess.call("sleep 0.2; chwb -s 8 -c 0xafafaf $(lsw)", shell=True)
-				subprocess.call("chwb2 -i 1 -o 9 -I " + double_border_colour + " -O $(xrdb -query | head -n 2 | tail -n 1 | cut -c 16-) $(lsw)", shell=True)
+				new_borders()
 	if ev.type == X.KeyRelease:
 		#key up
-		print("key up")
 		startkey = None
 
 	############################################### button events
 	if ev.type == X.ButtonPress and ev.child != X.NONE:
 		# button down
-		print("button down")
 		attr = ev.child.get_geometry()
 		startbutton = ev
 	if startbutton:
-		#dpy.screen().root.fill_rectangle(startbutton.child.create_gc(display=dpy), 10, 10, 300, 20)
 		# window moving
-		print('move')
 		xdiff = ev.root_x - startbutton.root_x
 		ydiff = ev.root_y - startbutton.root_y
+		if startbutton.detail == 3:
+			mouse_move = "resize"
+		else:
+			mouse_move = "drag"
+
 		startbutton.child.configure(
 			x = attr.x + (startbutton.detail == 1 and xdiff or 0),
 			y = attr.y + (startbutton.detail == 1 and ydiff or 0),
@@ -69,6 +75,6 @@ while 1:
 			height = max(1, attr.height + (startbutton.detail == 3 and ydiff or 0)))
 	if ev.type == X.ButtonRelease:
 		#button up
-		print("button up")
 		startbutton = None
-
+		if mouse_move == "resize":
+			new_borders()
